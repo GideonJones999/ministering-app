8ed8a396ccc4dccf6a8b3881aa00281bf24e9d3c
GideonJones999 <gideon.w.jones@gmail.com>
2025-05-20 19:03:07 -0600

    Updated Member Card

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import { Member } from "../data/exampleData";
+import { useDraggable } from "@dnd-kit/core";
 
 interface MemberCardProps {
   member: Member;
 }
 
-
 export default function MemberCard({ member }: MemberCardProps) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: member.id,
+  });
+
+  const style = {
+    transform: transform
+      ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
+      : undefined,
+    border: "1px solid #ccc",
+    borderRadius: "5em",
+  };
+
   return (
-    <div className="border rounded p-3 mb-2 bg-green-50">
-      <p className="font-semibold">{member.name}</p>
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="border rounded p-3 mb-2 bg-blue-50"
+    >
+      <h4 className="font-semibold">{member.name}</h4>
       <p className="text-sm text-gray-600">
         {member.isFamily ? "Family" : "Individual"}
       </p>
