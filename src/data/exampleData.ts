faf061fafeab42596349e36435d643eda4705137
GideonJones999 <gideon.w.jones@gmail.com>
2025-05-21 17:48:40 -0600

    remove dummy data

--- src/data/exampleData.ts
+++ src/data/exampleData.ts
@@ -14,20 +14,6 @@ export type Companionship = {
   members?: Member[];
 };
 
-export const ministers: Minister[] = [
-  { id: "min-m1", name: "John" },
-  { id: "min-m2", name: "Paul" },
-  { id: "min-m3", name: "George" },
-  { id: "min-m4", name: "Gideon" },
-  { id: "min-w1", name: "Mary" },
-  { id: "min-w2", name: "Sarah" },
-  { id: "min-w3", name: "Sailor" },
-  { id: "min-w4", name: "Eden" },
-];
+export const ministers: Minister[] = [];
 
-export const members: Member[] = [
-  { id: "mem-f1", name: "The Smith Family" },
-  { id: "mem-w3", name: "Anna" },
-  { id: "mem-f2", name: "The Johnson Family" },
-  { id: "mem-w4", name: "Rachel" },
-];
+export const members: Member[] = [];
